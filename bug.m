bf384ae96a668562af607db37ead2bb0a2ef85e6
BugHunter-X <askzenai@gmail.com>
2024-12-18 16:36:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,10 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+    return;
+  end
+  % More code here...
+end
+
+% This function will return early, potentially not executing the complete code.
